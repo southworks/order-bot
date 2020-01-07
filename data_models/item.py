b989d734fe3e44cbc35c769a0a7669f39e6eab25
Konigstiger <mariano.rodriguez@southworks.com>
2020-01-07 15:20:51 -0300

    Update item.py
    Changes:
    - Add quantity and unit properties in Item class.

--- data_models/item.py
+++ data_models/item.py
@@ -1,6 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from data_models.unit import Unit
+
+
 class Item:
-    def __init__(self, product_id: int = 0, description: str = None, item_id: id = 0, known_names: str = None):
+    """ TODO: Add description for Item class. """
+    def __init__(self, product_id: int = 0, description: str = None, item_id: id = 0, known_names: str = None,
+                 quantity: float = 0, unit: Unit = None):
         self.product_id = product_id
         self.description = description
         self.item_id = item_id
         self.known_names = known_names
+        self.quantity = quantity
+        self.unit = unit
+

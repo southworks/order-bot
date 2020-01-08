0ef54b0570082756b3048c7730620a35e56735d2
Konigstiger <mariano.rodriguez@southworks.com>
2020-01-08 10:50:12 -0300

    Update order.py
    Changes:
    - Add property status: OrderStatus
    - Add method show_items()
    - First implementation of method: add_item()
    - First implementation of method: remove_item()
    - Changed signature of constructor

--- data_models/order.py
+++ data_models/order.py
@@ -2,22 +2,31 @@
 # Licensed under the MIT License.
 
 import enum
-
+from typing import List
 from data_models.item import Item
 
 
 class Order:
-    def __init__(self, order_id: int = 0, item_list: [Item] = []):
+    def __init__(self, order_id: int = 0):
         self.order_id = order_id
-        self.item_list = item_list
+        self.item_list: List[Item] = list()
+        self.status: OrderStatus
 
+    # TODO: see if the parameter quantity is removed here
     def add_item(self, quantity, item):
         """ TODO: Add description for Order.add_item """
+        # TODO: Define the arithmetic, substitution or addition
+        item.quantity = quantity
+        self.item_list.append(item)
+        # TODO: Add return type
         return
 
     def remove_item(self, quantity, item):
         """ TODO: Add description for Order.remove_item """
-
+        # TODO: Define the arithmetic, substitution or subtraction
+        item.quantity = quantity
+        self.item_list.remove(item)
+        # TODO: Add return type
         return
 
     def confirm_order(self):
@@ -29,6 +38,17 @@ class Order:
         """ returns a text representation of the object """
         # TODO: Implement method Order.to_string
         return "{0}".format(self.order_id)
+
+    def show_items(self) -> str:
+        """ Returns the content of the list """
+        content = ""
+        if len(self.item_list) == 0:
+            content += "The list is empty."
+
+        for item in self.item_list:
+            content += item.to_string()
+
+        return content
 
 
 class OrderStatus(enum.Enum):

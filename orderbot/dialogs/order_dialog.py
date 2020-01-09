27650f2d78ebedca14b6750ec058806e058e6e6b
Giuliano Taliano <giuliano.taliano@southworks.com>
2020-01-09 13:31:47 -0300

    Add posibility to "confirm" the order (WIP)

--- orderbot/dialogs/order_dialog.py
+++ orderbot/dialogs/order_dialog.py
@@ -44,6 +44,7 @@ class OrderDialog(ComponentDialog):
         self.add_dialog(ChoicePrompt("options_step"))
         self.add_dialog(TextPrompt("interpret_user_intention"))
         self.add_dialog(ChoicePrompt(ChoicePrompt.__name__))
+        self.add_dialog(ConfirmPrompt(ConfirmPrompt.__name__))
         self.initial_dialog_id = WaterfallDialog.__name__
         self.add_dialog(TextPrompt(TextPrompt.__name__))
 
@@ -80,10 +81,10 @@ class OrderDialog(ComponentDialog):
             self.order_list.append(order)
             self.current_order = order
 
-        # Add the item to the list:
-        await step_context.context.send_activity(
-            MessageFactory.text("Adding two items")
-        )
+        # # Add the item to the list:
+        # await step_context.context.send_activity(
+        #     MessageFactory.text("Adding two items")
+        # )
 
         self.current_order.item_list.clear()
 
@@ -91,39 +92,39 @@ class OrderDialog(ComponentDialog):
         self.current_order.add_item(item2.quantity, item2)
         self.current_order.add_item(item3.quantity, item3)
 
-        lista_estado_1 = "The items in the list are:\n" + self.current_order.show_items()
-        # show list: 1 item
         await step_context.context.send_activity(
-            MessageFactory.text(lista_estado_1)
+            MessageFactory.text("You can Remove or Add an item to the list (Remove/Add 1 item)")
         )
-        print(lista_estado_1)
 
         await step_context.context.send_activity(
-            MessageFactory.text("Now, we remove items from item1 and item2, and add a few to item3")
+            MessageFactory.text("When you are ready to confirm the order, type 'Confirm order'")
         )
 
-        self.current_order.remove_item(2, item1)
-        self.current_order.remove_item(2, item2)
-        self.current_order.add_item(4, item3)
-
-        lista_estado_2 = "The items in the list are:\n" + self.current_order.show_items()
-
-        print(lista_estado_2)
-
-        await step_context.context.send_activity(
-             MessageFactory.text(lista_estado_2)
-        )
-
-        message_text = (
-            str(step_context.options)
-            if step_context.options
-            else "What can I help you with today?"
-        )
+        lista_estado = "The items in the list are:\n" + self.current_order.show_items()
 
         prompt_message = MessageFactory.text(
-            message_text, message_text
+            lista_estado
         )
 
+        #
+        # self.current_order.remove_item(2, item1)
+        # self.current_order.remove_item(2, item2)
+        # self.current_order.add_item(4, item3)
+        #
+        # lista_estado_2 = "The items in the list are:\n" + self.current_order.show_items()
+        #
+        # print(lista_estado_2)
+        #
+        # await step_context.context.send_activity(
+        #      MessageFactory.text(lista_estado_2)
+        # )
+        #
+        # message_text = (
+        #     str(step_context.options)
+        #     if step_context.options
+        #     else "What can I help you with today?"
+        # )
+        #
         return await step_context.prompt(
             TextPrompt.__name__, PromptOptions(prompt=prompt_message)
         )
@@ -135,107 +136,76 @@ class OrderDialog(ComponentDialog):
         step_context.values["input"] = step_context.result
         query = str(step_context.result)
 
-        splitted = query.split()
+        if "Confirm" not in query:
+            splitted = query.split()
 
-        # having the splitted array:
-        await step_context.context.send_activity(
-            MessageFactory.text("Your action is: " + splitted[0])
-        )
+            # having the splitted array:
+            # await step_context.context.send_activity(
+            #     MessageFactory.text("Your action is: " + splitted[0])
+            # )
+            #
+            # await step_context.context.send_activity(
+            #     MessageFactory.text("Your quantity is: " + splitted[1])
+            # )
+            #
+            # await step_context.context.send_activity(
+            #     MessageFactory.text("Your Item is: " + splitted[2])
+            # )
+            unit = Unit(1)
 
-        await step_context.context.send_activity(
-            MessageFactory.text("Your quantity is: " + splitted[1])
-        )
+            # I dont know if this is going to work
+            item = list(filter(lambda itm: itm.description == splitted[2], self.current_order.item_list))
+            if not item:
+                item = Item(product_id=self.current_order.item_list[-1].product_id + 1, description=splitted[2],
+                            item_id=self.current_order.item_list[-1].item_id + 1, quantity=int(splitted[1]), unit=unit)
+            if splitted[0] == 'Add':
+                self.current_order.add_item(float(splitted[1]), item[0] if type(item) is list else item)
+                await step_context.context.send_activity(
+                    MessageFactory.text(splitted[1] + " " + splitted[2] + " added!")
+                )
+            elif splitted[0] == 'Remove':
+                self.current_order.remove_item(float(splitted[1]), item[0])
+                await step_context.context.send_activity(
+                    MessageFactory.text(splitted[1] + " " + splitted[2] + " removed!")
+                )
 
-        await step_context.context.send_activity(
-            MessageFactory.text("Your Item is: " + splitted[2])
-        )
-        unit = Unit(1)
-        item_x = None
+            lista_estado_3 = "The items in the list are:\n" + self.current_order.show_items()
+            print(lista_estado_3)
 
-        # I dont know if this is going to work
-        item = list(filter(lambda itm: itm.description == splitted[2], self.current_order.item_list))
-        if not item:
-            item = Item(product_id=self.current_order.item_list[-1].product_id + 1, description=splitted[2],
-                        item_id=self.current_order.item_list[-1].item_id, quantity=int(splitted[1]), unit=unit)
-        if splitted[0] == 'Add':
-            self.current_order.add_item(float(splitted[1]), item[0] if type(item) is list else item)
-        elif splitted[0] == 'Remove':
-            self.current_order.remove_item(float(splitted[1]), item[0])
+            prompt_message = MessageFactory.text(lista_estado_3)
 
-        lista_estado_3 = "The items in the list are:\n" + self.current_order.show_items()
-        print(lista_estado_3)
+            await step_context.prompt(
+                TextPrompt.__name__, PromptOptions(prompt=prompt_message)
+            )
 
-        prompt_message = MessageFactory.text(lista_estado_3)
-
-        return await step_context.prompt(
-            TextPrompt.__name__, PromptOptions(prompt=prompt_message)
-        )
-
-        # if step_context.result.value == 'Portfolio':
-        #     await step_context.context.send_activity(
-        #         MessageFactory.text(f"Very well, this is your portfolio.")
-        #     )
-        #     # await step_context.context.send_activity(
-        #     #     MessageFactory.text("We could and should use a Card here.")
-        #     # )
-        #
-        #     # Verify that the operation object has ALL the info needed.
-        #     card = self.create_table_style_card()
-        #
-        #     response = create_activity_reply(
-        #         step_context.context.activity, "", "", [card]
-        #     )
-        #     await step_context.context.send_activity(response)
-        #
-        #     img_result = self.portfolio.show()
-        #
-        #     # Replace this text for a CARD
-        #     await step_context.context.send_activity(
-        #        MessageFactory.text(self.portfolio.show())
-        #     )
-        #
-        #     # review if this can be deleted and dependant methods too
-        #     # reply = Activity(type=ActivityTypes.message)
-        #     # reply.text = "Portfolio image."
-        #     # reply.attachments = [self.get_plot_img()]
-        #     # await step_context.context.send_activity(reply)
-        #
-        #     # Portfolio distribution (cake)
-        #     reply_cake = Activity(type=ActivityTypes.message)
-        #     reply_cake.text = "Portfolio Distribution."
-        #     reply_cake.attachments = [self.get_cake_img()]
-        #     await step_context.context.send_activity(reply_cake)
-        #
-        #     # Here, the conversation can continue, or be terminated and reset
-        #     return await step_context.end_dialog()
-        #
-        # elif step_context.result.value == 'Trade':
-        #     await step_context.context.send_activity(
-        #         MessageFactory.text(f"Ok, you want to trade.")
-        #     )
-        #     return await step_context.prompt(
-        #         "text_prompt_input",
-        #         PromptOptions(prompt=MessageFactory.text("What do you want to buy or sell?")),
-        #     )
-        #     # Here we wait for a TextPrompt input, that should contain the user intent,
-        #     # like:
-        #     # Buy 25 MSFT for $ 120
-        #
-        #     # here, we can also have a Choice Prompt based on our current holdings.
-        #     # that way, the user intent, is getting narrowed in a interactive fashion.
-        #
-        # elif step_context.result.value == 'Help':
-        #     await step_context.context.send_activity(
-        #         MessageFactory.text(f"Some day, when the sun is bright in the sky and all the backlog tasks are completed, I will be able to give you help. Sorry.")
-        #     )
+            await self.interpret_user_intention(step_context)
+        else:
+            return await step_context.prompt(
+                ConfirmPrompt.__name__,
+                PromptOptions(prompt=MessageFactory.text("You want to confirm this order?")),
+            )
 
     async def third_step(self, step_context: WaterfallStepContext) -> DialogTurnResult:
         """ TODO: Add description for OrderDialog.second_step """
-        return_response = "Bye!"
+        if not step_context.result:
+            await self.interpret_user_intention(step_context)
+        else:
+            await step_context.context.send_activity(
+                MessageFactory.text("The order was confirmed!")
+            )
+            await step_context.context.send_activity(
+                MessageFactory.text("Thank you!")
+            )
+            await step_context.context.send_activity(
+                MessageFactory.text("Bye!")
+            )
 
-        await step_context.context.send_activity(
-            MessageFactory.text(return_response)
-        )
-
-        return await step_context.end_dialog()
+            return await step_context.end_dialog()
+            # return_response = "Bye!"
+            #
+            # await step_context.context.send_activity(
+            #     MessageFactory.text(return_response)
+            # )
+            #
+            # return await step_context.end_dialog()
 

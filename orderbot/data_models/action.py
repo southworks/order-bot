6fffd1ddc6109a1861424e1594e1eeba1d79e5bc
Giuliano Taliano <giuliano.taliano@southworks.com>
2020-01-13 15:25:03 -0300

    Minor changes

--- orderbot/data_models/action.py
+++ orderbot/data_models/action.py
@@ -5,31 +5,33 @@ from orderbot.data_models.order import Order
 
 class Action:
     """ TODO: Add description for Action class. """
-    def execute(self, number, order, item):
+    def execute(self, quantity, weight, order, item):
         raise NotImplementedError
 
 
 class Add(Action):
     """ TODO: Add description for Add class. """
-    def execute(self, number, order: Order, item):
-        order.add_item(number, item)
+    def execute(self, quantity, weight, order: Order, item):
+        order.add_item(quantity, weight, item)
 
     @property
     def description(self):
         return 'added'
 
+
 class Remove(Action):
     """ TODO: Add description for Remove class. """
-    def execute(self, number, order: Order, item):
-        order.remove_item(number, item)
+    def execute(self, quantity, weight, order: Order, item):
+        order.remove_item(quantity,  weight, item)
 
     @property
     def description(self):
         return 'removed'
 
+
 class Confirm(Action):
     """ TODO: Add description for Confirm class. """
-    def execute(self, number, order: Order, item):
+    def execute(self, quantity, weight, order: Order, item):
         order.confirm_order()
 
     @property

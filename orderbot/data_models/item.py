4c0976ec80ea2d3414ca1de13a3684590fd65985
Giuliano Taliano <giuliano.taliano@southworks.com>
2020-01-13 15:24:06 -0300

    Add weight property and modify to_string method

--- orderbot/data_models/item.py
+++ orderbot/data_models/item.py
@@ -7,7 +7,7 @@ from .unit import Unit
 class Item:
     """ Represents a generic item of an Order. """
     def __init__(self, product_id: int = 0, description: str = None, item_id: id = 0, known_names: str = None,
-                 quantity: float = 0, unit: Unit = None):
+                 quantity: float = 0, weight: float = 0, unit: Unit = None):
         self.product_id = product_id
         self.description = description
         self.item_id = item_id
@@ -16,11 +16,14 @@ class Item:
         self.known_names = known_names
         self.quantity = quantity
         self.unit = unit
+        self.weigth = weight
 
     def to_string(self) -> str:
         """ returns a text representation of the Item """
         # TODO: check if the description is ""
         if self.unit.description == "":
-            return "{0} {1}".format(self.quantity, self.description)
-        else:
-            return "{0} {1} of {2}".format(self.quantity, self.unit.description, self.description)
+            return f"{self.quantity} {self.description.capitalize()}"
+        elif self.unit.description != "" and self.quantity == 0:
+            return f"{self.weigth}{self.unit.description} of {self.description.capitalize()}"
+        elif self.unit.description != "" and self.quantity != 0 and self.weigth != 0:
+            return f"{self.quantity} {self.description.capitalize()} of {self.weigth}{self.unit.description}"

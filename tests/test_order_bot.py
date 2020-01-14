df15a270695f6985570067adc5d3ed87c582e32c
aliandi <mica.andini@gmail.com>
2020-01-14 10:25:45 -0300

    Fix functional tests

--- tests/test_order_bot.py
+++ tests/test_order_bot.py
@@ -1,11 +1,11 @@
-from orderbot.data_models import Order, Item
+from data_models import Order, Item
 
 
 class TestOrderBot:
     @staticmethod
-    def test_dummy_test():
+    def test_adding_item():
         order = Order(0)
         item = Item(1, "Item1", 1)
         item.description = "item1"
         item.quantity = 1
-        order.add_item(1, item)
+        order.add_item(1, 0, item)

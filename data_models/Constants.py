93335081fccaff02a1d6e7533b88db8909f63d4a
Giuliano Taliano <giuliano.taliano@southworks.com>
2020-01-14 12:08:46 -0300

    Add Constants class and ConstantUnit class

---
+++ data_models/Constants.py
@@ -0,0 +1,12 @@
+number_type_name = 'number'
+dimension_type_name = 'dimension'
+
+
+class ConstantUnits:
+    units = [
+        'Kg', 'kg', 'Kilogram',
+        'Gr', 'gr', 'Gram',
+        'Mg', 'mg', 'Milligram',
+        'L', 'l', 'Liter',
+        'Ml', 'ml', 'Milliliter'
+    ]

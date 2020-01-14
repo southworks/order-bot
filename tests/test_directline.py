c949974196217abd6ccccf17b9bb4408290bcb20
aliandi <mica.andini@gmail.com>
2020-01-14 09:29:55 -0300

    Fix directline test

--- tests/test_directline.py
+++ tests/test_directline.py
@@ -20,7 +20,7 @@ from tests.directline_client import DirectLineClient
 class PyBotTest(TestCase):
     def setUp(self):
         direct_line_config = os.environ.get(
-            "DIRECT_LINE_CONFIG", "DirectLineConfig.json"
+            "DIRECT_LINE_CONFIG", "../DirectLineConfig.json"
         )
         with open(direct_line_config) as direct_line_file:
             self.direct_line_config = json.load(direct_line_file)
@@ -31,7 +31,8 @@ class PyBotTest(TestCase):
 
     def test_deployed_bot_answer(self):
         client = DirectLineClient(self.direct_line_secret)
-        user_message = "Contoso"
+        user_message = "Hi"
+        response_message = "The items in the list are:\n5 Chocolate\n3 Yerba\n2 Candy\n"
 
         send_result = client.send_message(user_message)
         self.assertIsNotNone(send_result)
@@ -40,5 +41,5 @@ class PyBotTest(TestCase):
         response, text = client.get_message()
         self.assertIsNotNone(response)
         self.assertEqual(200, response.status_code)
-        self.assertEqual(f"Echo: {user_message}", text)
+        self.assertEqual(response_message, text)
         print("SUCCESS!")

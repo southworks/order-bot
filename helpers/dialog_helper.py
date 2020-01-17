e862bb3a113328acf226b8c8131874aa08bbcc79
Giuliano Taliano <giuliano.taliano@southworks.com>
2020-01-17 11:48:57 -0300

    Fix item_description recognition issue

--- helpers/dialog_helper.py
+++ helpers/dialog_helper.py
@@ -17,38 +17,43 @@ class DialogHelper:
             return Confirm()
 
     @staticmethod
-    def normalize_description(has_unit, is_quantity, user_input, match):
+    def normalize_description(has_unit, is_quantity, user_input, unit, match):
+        splitted_input = user_input.split()
         item_description = ''
         if has_unit:
             if 'of' in user_input:
                 user_input = user_input.replace('of', '')
-            item_description = user_input[match.start + len(match.text):].strip()
+            item_description = user_input[match.end + len(match.text) + len(unit):].strip()
         elif is_quantity:
             user_input.strip()
-            item_description = user_input[match.start + len(match.text):].strip()
+            item_description = user_input[match.end + len(match.text):].strip()
+        else:
+            item_description = user_input[len(splitted_input[0]):].strip()
         return item_description
 
     @staticmethod
-    def resolve_quantity_and_weigh(match):
+    def resolve_quantity_and_weight(match):
         is_quantity = False
-        quantity = None
-        weight = None
+        quantity = 0
+        weight = 0
         has_unit = False
-        type_name = match.type_name
-        unit = ''
+        type_name = match.type_name if match else ''
+
         if type_name == Constants.number_type_name:
             if '.' in match.resolution.get('value'):
                 has_unit = True
+                quantity = 0
                 weight = float(match.resolution.get('value'))
             else:
                 is_quantity = True
+                weight = 0
                 quantity = int(match.resolution.get('value'))
         elif type_name == Constants.dimension_type_name:
             has_unit = True
             weight = float(match.resolution.get('value'))
-            unit = (match.resolution.get('unit'))
+            quantity = 0
 
-        return has_unit, weight, is_quantity, quantity, unit
+        return has_unit, weight, is_quantity, quantity
 
     @staticmethod
     def init_dialog():
